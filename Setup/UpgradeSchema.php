f3a75501453d5855ca084f2a1ed3c4b1f826a239
Patchara Lewchalermwong <39079735+GBPrimepay@users.noreply.github.com>
2020-08-12 09:28:03 +0700

    Public Release

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -7,6 +7,10 @@
 
 namespace GBPrimePay\Payments\Setup;
 
+
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
 use Magento\Framework\DB\Ddl\Table as Table;
 use GBPrimePay\Payments\Helper\Constant as Constant;
 
@@ -18,12 +22,12 @@ class UpgradeSchema implements \Magento\Framework\Setup\UpgradeSchemaInterface
     /**
      * DB setup code
      *
-     * @param \Magento\Framework\Setup\UpgradeSchemaInterface $setup
+     * @param \Magento\Framework\Setup\UpgradeSchemaInterface $upgrade
      * @param \Magento\Framework\Setup\ModuleContextInterface $context
      * @return void
      */
-    public function install(
-        \Magento\Framework\Setup\UpgradeSchemaInterface $setup,
+    public function upgrade(
+        \Magento\Framework\Setup\SchemaSetupInterface $setup,
         \Magento\Framework\Setup\ModuleContextInterface $context
     ) {
 
@@ -31,55 +35,56 @@ class UpgradeSchema implements \Magento\Framework\Setup\UpgradeSchemaInterface
         $_prefix = Constant::TABLE_PREFIX;
         $setup->startSetup();
 
-
-        $table = $setup->getConnection()->newTable(
-            $setup->getTable($_prefix . 'purchase')
-        )->addColumn(
-            'id',
-            Table::TYPE_INTEGER,
-            null,
-            ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
-            'ID'
-        )->addColumn(
-            'magento_customer_id',
-            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-            null,
-            [],
-            'Magento Customer ID'
-        )->addColumn(
-            'purchase_method',
-            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-            128,
-            [],
-            'Purchase Method'
-        )->addColumn(
-            'quoteid',
-            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-            128,
-            [
-                'unsigned' => true,
-                'nullable' => false,
-                'primary' => true
-            ],
-            'Quote ID'
-        )->addColumn(
-            'created_at',
-            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-            null,
-            ['default' => Table::TIMESTAMP_INIT],
-            'Create Time'
-        )->addColumn(
-            'status',
-            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-            10,
-            [
-                'nullable' => false,
-                'default' => 'active'
-            ],
-            'Purchase Status'
-        )->setComment(
-            'Purchase Order for GBPrimePay payment methods'
-        );
+        if (version_compare($version, '1.8.3') < 0) {
+          $table = $setup->getConnection()->newTable(
+              $setup->getTable($_prefix . 'purchase')
+          )->addColumn(
+              'id',
+              Table::TYPE_INTEGER,
+              null,
+              ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+              'ID'
+          )->addColumn(
+              'magento_customer_id',
+              \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+              null,
+              [],
+              'Magento Customer ID'
+          )->addColumn(
+              'purchase_method',
+              \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+              128,
+              [],
+              'Purchase Method'
+          )->addColumn(
+              'quoteid',
+              \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+              128,
+              [
+                  'unsigned' => true,
+                  'nullable' => false,
+                  'primary' => true
+              ],
+              'Quote ID'
+          )->addColumn(
+              'created_at',
+              \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+              null,
+              ['default' => Table::TIMESTAMP_INIT],
+              'Create Time'
+          )->addColumn(
+              'status',
+              \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+              10,
+              [
+                  'nullable' => false,
+                  'default' => 'active'
+              ],
+              'Purchase Status'
+          )->setComment(
+              'Purchase Order for GBPrimePay payment methods'
+          );
+        }
 
 
         $setup->getConnection()->createTable($table);
